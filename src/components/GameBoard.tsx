c85c38d2edcb4909f17c933c57372458d00088a0
Chris Vo <vochris3@gmail.com>
2023-04-25 22:50:04 -0400

    new types file and better new GameBoard component

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,5 +1,22 @@
-const GameBoard = () => {
-	// TODO: add more stores into this
+import * as React from 'react';
+import { useState } from 'react';
+import { useStore } from 'zustand';
+
+import { useGameStore } from '../store';
+
+const GameBoard: React.FC = () => {
+	// destructing store
+	const {
+		correctNumber,
+		guessedNumber,
+		score,
+		message,
+		setMessage,
+		setGuessedNumber,
+		setCorrectNumber,
+		setScore,
+	} = useStore(useGameStore);
+
 	return <></>;
 };
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,22 @@
 import create from 'zustand/vanilla';
+import type { Message } from './types';
 
 export type GameState = {
-	message: string;
+	message: Message;
 	correctNumber: number;
 	guessedNumber: number | null;
 	score: number;
 
-	setMessage: (message: string) => void;
+	setMessage: (message: Message) => void;
 	setCorrectNumber: () => void;
 	setGuessedNumber: (number: number | null) => void;
 	setScore: (score: number) => void;
 };
 
 export const useGameStore = create<GameState>((set) => ({
-	message: 'Start guessing...',
+	message: {
+		text: 'Start guessing...',
+	},
 	correctNumber: Math.trunc(Math.random() * 20) + 1,
 	guessedNumber: null,
 	score: 20,
